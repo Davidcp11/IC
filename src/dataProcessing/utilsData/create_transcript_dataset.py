b35e4c73d28704abfabf8b2902d028a173e92677
David Costa Pereira <pereiracostadavid@gmail.com>
2024-06-29 10:27:59 -0300

    Novas modificações

--- src/dataProcessing/utilsData/create_transcript_dataset.py
+++ src/dataProcessing/utilsData/create_transcript_dataset.py
@@ -33,12 +33,10 @@ def contar_indicadores(dataframe1, dataframe2, indicador, tipo=1):
     for i, row in dataframe1.iterrows():
         inicio = row['data']
         fim = row['prox_reuniao']
-        print(pd.isna(fim))
         if not pd.isna(fim):
             filtro = dataframe2[(dataframe2['Date'] >= inicio) & (dataframe2['Date'] <= fim) & (dataframe2[f'Indicator_{indicador}'] == tipo)]
         else:
             fim = inicio + pd.DateOffset(months=3)
-            print(fim)
             filtro = dataframe2[(dataframe2['Date'] >= inicio) & (dataframe2['Date'] <= fim) & (dataframe2[f'Indicator_{indicador}'] == tipo)]
         contagem.append(len(filtro))
     return contagem
@@ -46,6 +44,12 @@ def contar_indicadores(dataframe1, dataframe2, indicador, tipo=1):
 
 file_data_transcripts = os.listdir('../../dataset/transcripts')
 for file in file_data_transcripts:
+    if os.path.exists(f'../../dataset/transcripts_and_returns/{name_prices[file]}.csv'):
+        print("Transcript and returns file already exists for: " + name_prices[file])
+        continue
+    if not os.path.exists(f'../../dataset/transcripts/{file}/datas.csv'):
+        print("Data file does not exist: "+file)
+        continue
     df = pd.read_csv(f'../../dataset/transcripts/{file}/datas.csv')
     df = df.sort_values(by=['data'], ignore_index=True)
     df['data'] = pd.to_datetime(df['data'])
@@ -65,4 +69,6 @@ for file in file_data_transcripts:
     df['Month_Return_Neutral'] = contar_indicadores(df, prices, 'Month_Return', tipo=0)
     df['Month_Return_Negative'] = contar_indicadores(df, prices, 'Month_Return', tipo=-1)
     df['Month_Return_Total'] = df['Month_Return_Positive'] + df['Month_Return_Neutral'] + df['Month_Return_Negative']
-    df.to_csv(f'../../dataset/transcripts_and_returns/{name_prices[file]}.csv')
+    df.to_csv(f'../../dataset/transcripts_and_returns/{name_prices[file]}.csv', index=False)
+    print(f"Transcript and return created in: {name_prices[file]}.csv")
+
